1fbc16f587a690eabade5a348244edf8ad043683
3553x <3553x@tuta.io>
2017-03-17 00:31:17 +0000

    Update for testing

--- CTFpy/settings.py
+++ CTFpy/settings.py
@@ -25,8 +25,13 @@ SECRET_KEY = '78#!j)75@&niz=wh&=a^x)_bc#(_mvf%6^+s*@qr+@5ht+pgxt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [] #['127.0.0.1']
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+#SECURE_SSL_REDIRECT = True
+#SESSION_COOKIE_SECURE = True
+#CSRF_COOKIE_SECURE = True
 
 # Application definition
 
